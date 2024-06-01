d258f019e32f146388f6b27fa4d3b32443746551
varshil2005 <varshilkodinariya2005@gmail.com>
2024-06-01 19:00:01 +0530

    31_05_2024_homepage

--- app/homepage/Homepage.js
+++ app/homepage/Homepage.js
@@ -1,40 +1,80 @@
 import { View, Text, ImageBackground, StatusBar, TouchableOpacity, ScrollView, Image, StyleSheet, FlatList } from 'react-native'
 import React from 'react'
 import { horizontalScale, moderateScale, verticalScale } from '../../assets/metrics/Metrics'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Data = [
     {
         id: 1,
         title: 'Dorothy Perkins',
         subtitle: 'Evening  Dress',
-        image: require('../../assets/image/pic1.png'),
+        image: require('../../assets/image/imges2.webp'),
         price: 20,
         disscount: 10
 
     },
     {
         id: 2,
-        title:'Dorothy Perkins',
-        subtitle:'Evening  Dress',
-        image: require('../../assets/image/pic1.png'),
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/imges2.webp'),
         price: 20,
         disscount: 10
 
     },
     {
         id: 3,
-        title:'Dorothy Perkins',
-        subtitle:'Evening  Dress',
-        image: require('../../assets/image/pic1.png'),
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/imges2.webp'),
         price: 20,
         disscount: 10
 
     },
     {
         id: 4,
-        title:'Dorothy Perkins',
-        subtitle:'Evening  Dress',
-        image: require('../../assets/image/pic1.png'),
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/imges2.webp'),
+        price: 20,
+        disscount: 10
+
+    }
+]
+
+const newData = [ 
+    {
+        id: 1,
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/newproduct.jpg'),
+        price: 20,
+        disscount: 10
+
+    },
+    {
+        id: 2,
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/newproduct.jpg'),
+        price: 20,
+        disscount: 10
+
+    },
+    {
+        id: 3,
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/newproduct.jpg'),
+        price: 20,
+        disscount: 10
+
+    },
+    {
+        id: 4,
+        title: 'Dorothy Perkins',
+        subtitle: 'Evening  Dress',
+        image: require('../../assets/image/newproduct.jpg'),
         price: 20,
         disscount: 10
 
@@ -45,112 +85,104 @@ const Data = [
 
 export default function Homepage() {
 
-    const ProductCart = ({v}) => (
-        <View style={{ marginHorizontal: horizontalScale(10)}}>
-        <View>
-            <Image source={v.image} style={style.product}></Image>
-            <Text style={{ fontSize: 11 , marginHorizontal : horizontalScale(10)}}>{v.title}</Text>
-            <Text style={{ fontSize: 16, color: 'black' , marginHorizontal : horizontalScale(10)}}>{v.subtitle}</Text>
-            <Text style={{marginHorizontal : horizontalScale(10)}}>{v.disscount}</Text>
-        </View>
+    const ProductCart = ({ v }) => (
+        <View >
+            <View style={{ marginHorizontal: horizontalScale(14) }}>
+                <Image source={v.image} style={style.product}></Image>
+                <View style={style.rating}>
+                    <MaterialIcons name="star-rate" size={20} color='#FFBA49'></MaterialIcons>
+                    <MaterialIcons name="star-rate" size={20} color='#FFBA49'></MaterialIcons>
+                    <MaterialIcons name="star-rate" size={20} color='#FFBA49'></MaterialIcons>
+                    <MaterialIcons name="star-rate" size={20} color='#FFBA49'></MaterialIcons>
+                    <MaterialIcons name="star-rate" size={20} color='#FFBA49'></MaterialIcons>
+                </View>
+                <Text style={{ fontSize: 11 }}>{v.title}</Text>
+                <Text style={{ fontSize: 16, color: 'black' }}>{v.subtitle}</Text>
+                <View style = {style.priceview}>
+                <Text style = {style.pricetext}>{v.price}$</Text>
+                <Text style = {style.disscounttext}>{v.disscount}$</Text>
+                </View>
+               
+            </View>
         </View>
     )
 
 
     return (
-        <ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        >
             <StatusBar
-
                 barStyle="dark-content"
-                translucent backgroundColor='transparent'
-
+                translucent backgroundColor='transparent' 
             />
 
             <ImageBackground
-                style={{ width: '100%', height: verticalScale(550), position: 'relative' }}
-                source={require('../../assets/image/pexels-godisable-jacob-226636-896293.jpg')}
-
-            >
-
+                style={style.backgroundimage}
+                source={require('../../assets/image/pexels-godisable-jacob-226636-896293.jpg')}>
             </ImageBackground>
 
-            <View
-                style={{ width: '70%' }}
-            >
-                <Text
-                    style={{
-                        position: 'absolute',
-                        bottom: moderateScale(130),
-                        marginHorizontal: horizontalScale(15),
-                        color: 'white',
-                        fontSize: moderateScale(48),
-                        fontFamily: 'Metropolis-Black'
-                    }}
+            <View style={{ width: '70%' }}>
+                <Text style={style.title}>Fashion Sale</Text>
 
-                >Fashion Sale</Text>
-
-                <TouchableOpacity
-                    style={{
-                        position: 'absolute',
-                        bottom: moderateScale(75),
-                        backgroundColor: '#DB3022',
-                        marginHorizontal: horizontalScale(15),
-                        width: '60%',
-                        paddingVertical: verticalScale(12),
-                        borderRadius: moderateScale(20)
-                    }}
-                >
-                    <Text style={{ color: 'white', textAlign: 'center', fontFamily: 'Metropolis-Regular' }}>Check</Text>
+                <TouchableOpacity style={style.checkbutton}>
+                    <Text style={style.checktext}>Check</Text>
                 </TouchableOpacity>
+
             </View>
 
             <View>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        marginHorizontal: horizontalScale(15),
-                        marginVertical: verticalScale(20),
-                        alignItems: 'center'
-
-                    }}
-                >
+                <View style={style.saleview}>
                     <View>
-                        <Text
-                            style={{
-                                fontSize: 34,
-                                fontFamily: 'Metropolis-Black',
-                                color: 'black'
-                            }}
-                        >Sale</Text>
+                        <Text style={style.saletext}>Sale</Text>
                         <Text style={{ fontSize: 11, fontFamily: 'Metropolis-Regular' }}>Super summer sale</Text>
                     </View>
 
-                    <Text
-                        style={{
-                            fontFamily: 'Metropolis-Regular',
-                            fontSize: 13,
-                            color: 'black'
-                        }}
-                    >View All</Text>
+                    <Text style={style.viewalltext}>View All</Text>
                 </View>
 
-                
-                    <FlatList
 
-                        data={Data}
-                        renderItem={({ item }) => <ProductCart v={item} />}
-                        keyExtractor ={item => item.id}
-                        horizontal={true}
-                    />
-              
+                <FlatList
+
+                    data={Data}
+                    renderItem={({ item }) => <ProductCart v={item} />}
+                    keyExtractor={item => item.id}
+                    horizontal={true}
+                />
+
             </View>
+
+            <View>
+                <View style={style.saleview}>
+                    <View>
+                        <Text style={style.saletext}>New</Text>
+                        <Text style={{ fontSize: 11, fontFamily: 'Metropolis-Regular' }}>You've never seen it before!</Text>
+                    </View>
+
+                    <Text style={style.viewalltext}>View All</Text>
+                </View>
+
+
+                <FlatList
+
+                    data={newData}
+                    renderItem={({ item }) => <ProductCart v={item} />}
+                    keyExtractor={item => item.id}
+                    horizontal={true}
+                />
+
+            </View>
+
         </ScrollView>
     )
 }
 
 
 const style = StyleSheet.create({
+
+    container :{
+        marginHorizontal : horizontalScale(16)
+    },
 
     product: {
         width: horizontalScale(148),
@@ -162,5 +194,79 @@ const style = StyleSheet.create({
         position: 'relative',
         borderRadius: moderateScale(8),
         marginBottom: 5
+    },
+
+    rating: {
+        flexDirection: 'row',
+        color: '#FFBA49',
+        marginBottom : 8
+    },
+
+    backgroundimage: {
+        width: '100%',
+        height: verticalScale(550),
+        position: 'relative'
+    },
+
+    title: {
+        position: 'absolute',
+        bottom: moderateScale(130),
+        marginHorizontal: horizontalScale(15),
+        color: 'white',
+        fontSize: moderateScale(48),
+        fontFamily: 'Metropolis-Black'
+    },
+
+    checkbutton: {
+        position: 'absolute',
+        bottom: moderateScale(75),
+        backgroundColor: '#DB3022',
+        marginHorizontal: horizontalScale(15),
+        width: '60%',
+        paddingVertical: verticalScale(12),
+        borderRadius: moderateScale(20)
+    },
+
+    checktext: {
+        color: 'white',
+        textAlign: 'center',
+        fontFamily: 'Metropolis-Regular'
+    },
+
+    saleview: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: horizontalScale(15),
+        marginVertical: verticalScale(20),
+        alignItems: 'center'
+    },
+
+    saletext: {
+        fontSize: 34,
+        fontFamily: 'Metropolis-Black',
+        color: 'black'
+    },
+
+    viewalltext : {
+        fontFamily: 'Metropolis-Regular',
+        fontSize: 13,
+        color: 'black'
+    },
+
+    priceview : {
+        flexDirection : 'row'
+    },
+
+    pricetext : {
+        textDecorationLine : 'line-through',
+        fontFamily : 'Metropolis-Regular'
+    },
+
+    disscounttext : {
+        marginLeft :9,
+        color :'red',
+        fontFamily : 'Metropolis-Regular'
     }
+
+
 })
